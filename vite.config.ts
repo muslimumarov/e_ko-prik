b2853c2181c8fddefa5e6b6793f200be360b9604
muslimumarov <norbekumarov613@gmail.com>
2025-06-19 10:23:40 +0500

    login tesdtdfef

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: true,
-    proxy: {
-      "/api": {
-        target: "https://e-bridge.kuprikqurilish.uz",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      open: true,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_PROXY, // faqat developmentda ishlatiladi
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
+  };
 });
