c46ae9932d4aa3681874b86a0532fcbf606e0586
muslimumarov <norbekumarov613@gmail.com>
2025-06-26 16:58:26 +0500

    sayt test rejimida ishlamod

--- src/TestModeBanner.tsx
+++ src/TestModeBanner.tsx
@@ -1,7 +1,7 @@
 const TestLoader = () => {
   return (
-    <div className="fixed right-6 top-24 z-[9999] flex animate-bounce items-center gap-2 rounded-full bg-orange-100 px-4 py-2 text-gray-800 shadow-lg">
-      <div className="size-5 animate-spin rounded-full border-2 border-blue-500 border-t-transparent"></div>
+    <div className="fixed  bottom-5 right-5 z-[9999] flex animate-bounce items-center gap-2 rounded-full bg-orange-100 px-4 py-2 text-gray-800 shadow-lg">
+      <div className="size-4 animate-spin rounded-full border-2 border-blue-500 border-t-transparent"></div>
       <span className="text-sm font-medium">Sayt test rejimida ishlamoqda</span>
     </div>
   );
