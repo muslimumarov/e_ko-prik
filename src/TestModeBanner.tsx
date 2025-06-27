c5cec7e8a7dfa44493581235efffe653fdb580e5
muslimumarov <norbekumarov613@gmail.com>
2025-06-27 11:48:58 +0500

    loader

--- src/TestModeBanner.tsx
+++ src/TestModeBanner.tsx
@@ -1,8 +1,12 @@
+import { useTranslation } from "react-i18next";
+
 const TestLoader = () => {
+  const { t } = useTranslation();
+
   return (
-    <div className="fixed  bottom-5 right-5 z-[9999] flex animate-bounce items-center gap-2 rounded-full bg-orange-100 px-4 py-2 text-gray-800 shadow-lg">
-      <div className="size-4 animate-spin rounded-full border-2 border-blue-500 border-t-transparent"></div>
-      <span className="text-sm font-medium">Sayt test rejimida ishlamoqda</span>
+    <div className="fixed  bottom-5 right-5 z-[9999] flex animate-bounce items-center gap-2 rounded-full px-4 py-2 text-pink-500 shadow-lg backdrop-blur dark:text-white">
+      <div className="size-4 animate-spin rounded-full border-2 border-red-700  border-t-transparent"></div>
+      <span className="text-sm font-medium">{t("test")}</span>
     </div>
   );
 };
