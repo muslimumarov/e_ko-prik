1af07db5155aca3aa87908d82ddefd1748908402
muslimumarov <norbekumarov613@gmail.com>
2025-07-07 12:12:02 +0500

    fayllar tartiblangan

--- src/TestModeBanner.tsx
+++ src/TestModeBanner.tsx
@@ -4,7 +4,7 @@ const TestLoader = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="fixed  bottom-5 right-5 z-[9999] flex animate-bounce items-center gap-2 rounded-full px-4 py-2 text-pink-500 shadow-lg backdrop-blur dark:text-white">
+    <div className="fixed  bottom-0 right-5 z-[9999] flex animate-bounce items-center gap-2 rounded-full px-4 py-2 text-pink-500 shadow-lg backdrop-blur dark:text-white">
       <div className="size-4 animate-spin rounded-full border-2 border-red-700  border-t-transparent"></div>
       <span className="text-sm font-medium">{t("test")}</span>
     </div>
